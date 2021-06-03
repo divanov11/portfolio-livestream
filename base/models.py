c283e70ad79272d132e857f814c5c122306ff295
divanov11 <ivanovsin11@gmail.com>
2021-06-03 19:39:48 -0400

    Skill Icons

--- base/models.py
+++ base/models.py
@@ -31,6 +31,7 @@ class Comment(models.Model):
 class Skill(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField(null=True, blank=True)
+    logo = models.ImageField(null=True, default="skill.png")
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4,  unique=True,
                           primary_key=True, editable=False)
