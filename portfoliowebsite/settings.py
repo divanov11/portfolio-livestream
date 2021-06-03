089a9f5af191e3eb65b14bb3dea92494a2106dc6
divanov11 <ivanovsin11@gmail.com>
2021-06-03 16:42:20 -0400

    Added white noise

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
